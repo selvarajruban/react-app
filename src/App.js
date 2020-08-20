5fdc9c8e832e101deec1e092ccb57e904053ecdc
selvarajruban <selvarajruban@gmail.com>
2020-08-20 11:09:11 +0800

    Packages updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomepageImage from './components/HomePageImage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <HomepageImage />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Welcome to React world</code>
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click to Submit
         </a>
       </header>
     </div>
