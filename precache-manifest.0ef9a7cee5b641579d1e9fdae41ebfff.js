e0445fc1863f33553764983b7c69229455c92cd7
selvarajruban <selvarajruban@gmail.com>
2020-08-20 11:50:54 +0800

    Updates

---
+++ precache-manifest.0ef9a7cee5b641579d1e9fdae41ebfff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929abc7ee3add666eca8d3ddd85fe696",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "badd2fd7dae5ad1ffbf2",
+    "url": "/react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b8dc912edce1cb3e0ff",
+    "url": "/react-app/static/js/2.3e2e52e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.3e2e52e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "badd2fd7dae5ad1ffbf2",
+    "url": "/react-app/static/js/main.7c34601e.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  }
+]);
